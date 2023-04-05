ed7acec71ff30f44f2692e548d08da81ff7c27fd
PhuVuLuan <phuvuluan010101@gmail.com>
2023-04-05 22:31:55 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -36,10 +36,21 @@
                     <i class="fas fa-address-card"></i>
                 </h4>
                 <ContactCard :contact="activeContact" />
+                <router-link
+                        :to="{
+                        name: 'contact.edit',
+                        params: { id: activeContact._id },
+                        }"
+                    >
+                    <span class="mt-2 badge badge-warning">
+                        <i class="fas fa-edit"></i> Hiệu chỉnh
+                    </span>
+                </router-link>
             </div>
         </div>
     </div>
-    </template>
+</template>
+
 <script>
     import ContactCard from "@/components/ContactCard.vue";
     import InputSearch from "@/components/InputSearch.vue";
